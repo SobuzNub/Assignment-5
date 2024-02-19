e2a4e7e4efa8342fd7f57ae6bb7d6f033133fc86
Sobuz_Nub <khanshobuz99@gmail.com>
2024-02-19 22:09:01 +0600

    javascript added

---
+++ scripts/ph-paripahan.js
@@ -0,0 +1,202 @@
+
+
+
+
+function buttonAdd(){
+    const btn = document.getElementById('a1-button');
+    btn.style.backgroundColor = '#1DD100';
+   
+    const currentSeatElement = document.getElementById('current-seat');
+    const currentScoreText = currentSeatElement.innerText;
+    const currentSeat = parseInt(currentScoreText);
+    console.log(currentSeat)
+
+    const newSeat = currentSeat + 1;
+    currentSeatElement.innerText = newSeat;
+
+    
+
+    const currentSeatLeft = document.getElementById('seat-left');
+    const currentScoreSeat = currentSeatLeft.innerText;
+    const currentLeft = parseInt(currentScoreSeat);
+    console.log(currentLeft);
+
+    const seatLeft = currentLeft - 1;
+    currentSeatLeft.innerText = seatLeft;
+
+}
+
+function buttonAddTwo(){
+    const btn = document.getElementById('a2-button');
+    btn.style.backgroundColor = '#1DD100';
+   
+    const currentSeatElement = document.getElementById('current-seat');
+    const currentScoreText = currentSeatElement.innerText;
+    const currentSeat = parseInt(currentScoreText);
+    console.log(currentSeat)
+
+    const newSeat = currentSeat + 1;
+    currentSeatElement.innerText = newSeat;
+
+
+    const currentSeatLeft = document.getElementById('seat-left');
+    const currentScoreSeat = currentSeatLeft.innerText;
+    const currentLeft = parseInt(currentScoreSeat);
+    console.log(currentLeft);
+
+    const seatLeft = currentLeft - 1;
+    currentSeatLeft.innerText = seatLeft;
+
+}
+
+function buttonAddThree(){
+    const btn = document.getElementById('a3-button');
+    btn.style.backgroundColor = '#1DD100';
+   
+    const currentSeatElement = document.getElementById('current-seat');
+    const currentScoreText = currentSeatElement.innerText;
+    const currentSeat = parseInt(currentScoreText);
+    console.log(currentSeat)
+
+    const newSeat = currentSeat + 1;
+    currentSeatElement.innerText = newSeat;
+
+
+    const currentSeatLeft = document.getElementById('seat-left');
+    const currentScoreSeat = currentSeatLeft.innerText;
+    const currentLeft = parseInt(currentScoreSeat);
+    console.log(currentLeft);
+
+    const seatLeft = currentLeft - 1;
+    currentSeatLeft.innerText = seatLeft;
+
+}
+
+function buttonAddFour(){
+    const btn = document.getElementById('a4-button');
+    btn.style.backgroundColor = '#1DD100';
+   
+    const currentSeatElement = document.getElementById('current-seat');
+    const currentScoreText = currentSeatElement.innerText;
+    const currentSeat = parseInt(currentScoreText);
+    console.log(currentSeat)
+
+    const newSeat = currentSeat + 1;
+    currentSeatElement.innerText = newSeat;
+
+
+    const currentSeatLeft = document.getElementById('seat-left');
+    const currentScoreSeat = currentSeatLeft.innerText;
+    const currentLeft = parseInt(currentScoreSeat);
+    console.log(currentLeft);
+
+    const seatLeft = currentLeft - 1;
+    currentSeatLeft.innerText = seatLeft;
+
+}
+
+function buttonAddB1(){
+    const btn = document.getElementById('b1-button');
+    btn.style.backgroundColor = '#1DD100';
+   
+    const currentSeatElement = document.getElementById('current-seat');
+    const currentScoreText = currentSeatElement.innerText;
+    const currentSeat = parseInt(currentScoreText);
+    console.log(currentSeat)
+
+    const newSeat = currentSeat + 1;
+    currentSeatElement.innerText = newSeat;
+
+
+    const currentSeatLeft = document.getElementById('seat-left');
+    const currentScoreSeat = currentSeatLeft.innerText;
+    const currentLeft = parseInt(currentScoreSeat);
+    console.log(currentLeft);
+
+    const seatLeft = currentLeft - 1;
+    currentSeatLeft.innerText = seatLeft;
+
+}
+
+function buttonAddB2(){
+    const btn = document.getElementById('b2-button');
+    btn.style.backgroundColor = '#1DD100';
+   
+    const currentSeatElement = document.getElementById('current-seat');
+    const currentScoreText = currentSeatElement.innerText;
+    const currentSeat = parseInt(currentScoreText);
+    console.log(currentSeat)
+
+    const newSeat = currentSeat + 1;
+    currentSeatElement.innerText = newSeat;
+
+
+    const currentSeatLeft = document.getElementById('seat-left');
+    const currentScoreSeat = currentSeatLeft.innerText;
+    const currentLeft = parseInt(currentScoreSeat);
+    console.log(currentLeft);
+
+    const seatLeft = currentLeft - 1;
+    currentSeatLeft.innerText = seatLeft;
+
+}
+
+function buttonAddB3(){
+    const btn = document.getElementById('b3-button');
+    btn.style.backgroundColor = '#1DD100';
+   
+    const currentSeatElement = document.getElementById('current-seat');
+    const currentScoreText = currentSeatElement.innerText;
+    const currentSeat = parseInt(currentScoreText);
+    console.log(currentSeat)
+
+    const newSeat = currentSeat + 1;
+    currentSeatElement.innerText = newSeat;
+
+
+    const currentSeatLeft = document.getElementById('seat-left');
+    const currentScoreSeat = currentSeatLeft.innerText;
+    const currentLeft = parseInt(currentScoreSeat);
+    console.log(currentLeft);
+
+    const seatLeft = currentLeft - 1;
+    currentSeatLeft.innerText = seatLeft;
+
+}
+
+function buttonAddB4(){
+    const btn = document.getElementById('b4-button');
+    btn.style.backgroundColor = '#1DD100';
+   
+    const currentSeatElement = document.getElementById('current-seat');
+    const currentScoreText = currentSeatElement.innerText;
+    const currentSeat = parseInt(currentScoreText);
+    console.log(currentSeat)
+
+    const newSeat = currentSeat + 1;
+    currentSeatElement.innerText = newSeat;
+
+
+    const currentSeatLeft = document.getElementById('seat-left');
+    const currentScoreSeat = currentSeatLeft.innerText;
+    const currentLeft = parseInt(currentScoreSeat);
+    console.log(currentLeft);
+
+    const seatLeft = currentLeft - 1;
+    currentSeatLeft.innerText = seatLeft;
+
+}
+
+function next(){
+    const mainScreen = document.getElementById('next-btn');
+    mainScreen.classList.add('hidden');
+
+    const successScreen = document.getElementById('success-screen');
+    successScreen.classList.remove('hidden')
+}
+
+
+
+
+
+
---
+++ scripts/utility.js
@@ -0,0 +1,4 @@
+function addBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
