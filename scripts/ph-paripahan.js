0b21541da14ef4f0ee3f5433cf392ba1843e9826
Sobuz_Nub <khanshobuz99@gmail.com>
2024-02-22 22:35:36 +0600

    recheck

--- scripts/ph-paripahan.js
+++ scripts/ph-paripahan.js
@@ -2,201 +2,75 @@
 
 
 
-function buttonAdd(){
-    const btn = document.getElementById('a1-button');
-    btn.style.backgroundColor = '#1DD100';
-   
-    const currentSeatElement = document.getElementById('current-seat');
-    const currentScoreText = currentSeatElement.innerText;
-    const currentSeat = parseInt(currentScoreText);
-    console.log(currentSeat)
 
-    const newSeat = currentSeat + 1;
-    currentSeatElement.innerText = newSeat;
+
+const allBtn = document.getElementsByClassName('btn');
+
+for(const btn of allBtn){
+    btn.addEventListener("click", function(event){
+        const name = event.target.parentNode.childNodes[1].innerText;
+
+        const p = document.createElement("p");
+        const text = "Economy";
+        p.innerText = text;
+        const price = document.createElement("p");
+        const priceText = "550";
+        price.innerText = priceText;
+
+        const selectedContainers = document.getElementById('selected-seat-buttons');
+
+        const div = document.createElement("div");
+        div.classList.add("selected-seat");
+
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+        const p3 = document.createElement("p");
+        p1.innerText = name;
+        p2.innerText = text;
+        p3.innerText = priceText;
+
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        selectedContainers.appendChild(div);
+
+        
+        updateTotalCost(priceText)
+        updateGrandTotal()
+    })
+}
+
+
+
+function updateGrandTotal(){
+    const totalCost = getConvertedValue('total');
+
+    document.getElementById('grand-total').innerText = totalCost;
+} 
+
+
+
+
+function updateTotalCost(value){
+    
+    const totalCost = getConvertedValue('total');
+    const sum = totalCost + parseInt(value);
+    document.getElementById('total').innerText = sum;
+}
+
+
+
+
+
+
+function getConvertedValue(id){
+    const price = document.getElementById(id).innerText;
+    const convertedPrice = parseInt(price);
+    return convertedPrice;
+
+}
+
+
 
     
-
-    const currentSeatLeft = document.getElementById('seat-left');
-    const currentScoreSeat = currentSeatLeft.innerText;
-    const currentLeft = parseInt(currentScoreSeat);
-    console.log(currentLeft);
-
-    const seatLeft = currentLeft - 1;
-    currentSeatLeft.innerText = seatLeft;
-
-}
-
-function buttonAddTwo(){
-    const btn = document.getElementById('a2-button');
-    btn.style.backgroundColor = '#1DD100';
-   
-    const currentSeatElement = document.getElementById('current-seat');
-    const currentScoreText = currentSeatElement.innerText;
-    const currentSeat = parseInt(currentScoreText);
-    console.log(currentSeat)
-
-    const newSeat = currentSeat + 1;
-    currentSeatElement.innerText = newSeat;
-
-
-    const currentSeatLeft = document.getElementById('seat-left');
-    const currentScoreSeat = currentSeatLeft.innerText;
-    const currentLeft = parseInt(currentScoreSeat);
-    console.log(currentLeft);
-
-    const seatLeft = currentLeft - 1;
-    currentSeatLeft.innerText = seatLeft;
-
-}
-
-function buttonAddThree(){
-    const btn = document.getElementById('a3-button');
-    btn.style.backgroundColor = '#1DD100';
-   
-    const currentSeatElement = document.getElementById('current-seat');
-    const currentScoreText = currentSeatElement.innerText;
-    const currentSeat = parseInt(currentScoreText);
-    console.log(currentSeat)
-
-    const newSeat = currentSeat + 1;
-    currentSeatElement.innerText = newSeat;
-
-
-    const currentSeatLeft = document.getElementById('seat-left');
-    const currentScoreSeat = currentSeatLeft.innerText;
-    const currentLeft = parseInt(currentScoreSeat);
-    console.log(currentLeft);
-
-    const seatLeft = currentLeft - 1;
-    currentSeatLeft.innerText = seatLeft;
-
-}
-
-function buttonAddFour(){
-    const btn = document.getElementById('a4-button');
-    btn.style.backgroundColor = '#1DD100';
-   
-    const currentSeatElement = document.getElementById('current-seat');
-    const currentScoreText = currentSeatElement.innerText;
-    const currentSeat = parseInt(currentScoreText);
-    console.log(currentSeat)
-
-    const newSeat = currentSeat + 1;
-    currentSeatElement.innerText = newSeat;
-
-
-    const currentSeatLeft = document.getElementById('seat-left');
-    const currentScoreSeat = currentSeatLeft.innerText;
-    const currentLeft = parseInt(currentScoreSeat);
-    console.log(currentLeft);
-
-    const seatLeft = currentLeft - 1;
-    currentSeatLeft.innerText = seatLeft;
-
-}
-
-function buttonAddB1(){
-    const btn = document.getElementById('b1-button');
-    btn.style.backgroundColor = '#1DD100';
-   
-    const currentSeatElement = document.getElementById('current-seat');
-    const currentScoreText = currentSeatElement.innerText;
-    const currentSeat = parseInt(currentScoreText);
-    console.log(currentSeat)
-
-    const newSeat = currentSeat + 1;
-    currentSeatElement.innerText = newSeat;
-
-
-    const currentSeatLeft = document.getElementById('seat-left');
-    const currentScoreSeat = currentSeatLeft.innerText;
-    const currentLeft = parseInt(currentScoreSeat);
-    console.log(currentLeft);
-
-    const seatLeft = currentLeft - 1;
-    currentSeatLeft.innerText = seatLeft;
-
-}
-
-function buttonAddB2(){
-    const btn = document.getElementById('b2-button');
-    btn.style.backgroundColor = '#1DD100';
-   
-    const currentSeatElement = document.getElementById('current-seat');
-    const currentScoreText = currentSeatElement.innerText;
-    const currentSeat = parseInt(currentScoreText);
-    console.log(currentSeat)
-
-    const newSeat = currentSeat + 1;
-    currentSeatElement.innerText = newSeat;
-
-
-    const currentSeatLeft = document.getElementById('seat-left');
-    const currentScoreSeat = currentSeatLeft.innerText;
-    const currentLeft = parseInt(currentScoreSeat);
-    console.log(currentLeft);
-
-    const seatLeft = currentLeft - 1;
-    currentSeatLeft.innerText = seatLeft;
-
-}
-
-function buttonAddB3(){
-    const btn = document.getElementById('b3-button');
-    btn.style.backgroundColor = '#1DD100';
-   
-    const currentSeatElement = document.getElementById('current-seat');
-    const currentScoreText = currentSeatElement.innerText;
-    const currentSeat = parseInt(currentScoreText);
-    console.log(currentSeat)
-
-    const newSeat = currentSeat + 1;
-    currentSeatElement.innerText = newSeat;
-
-
-    const currentSeatLeft = document.getElementById('seat-left');
-    const currentScoreSeat = currentSeatLeft.innerText;
-    const currentLeft = parseInt(currentScoreSeat);
-    console.log(currentLeft);
-
-    const seatLeft = currentLeft - 1;
-    currentSeatLeft.innerText = seatLeft;
-
-}
-
-function buttonAddB4(){
-    const btn = document.getElementById('b4-button');
-    btn.style.backgroundColor = '#1DD100';
-   
-    const currentSeatElement = document.getElementById('current-seat');
-    const currentScoreText = currentSeatElement.innerText;
-    const currentSeat = parseInt(currentScoreText);
-    console.log(currentSeat)
-
-    const newSeat = currentSeat + 1;
-    currentSeatElement.innerText = newSeat;
-
-
-    const currentSeatLeft = document.getElementById('seat-left');
-    const currentScoreSeat = currentSeatLeft.innerText;
-    const currentLeft = parseInt(currentScoreSeat);
-    console.log(currentLeft);
-
-    const seatLeft = currentLeft - 1;
-    currentSeatLeft.innerText = seatLeft;
-
-}
-
-function next(){
-    const mainScreen = document.getElementById('next-btn');
-    mainScreen.classList.add('hidden');
-
-    const successScreen = document.getElementById('success-screen');
-    successScreen.classList.remove('hidden')
-}
-
-
-
-
-
 
